7064353a965eed6f7c00745ad685a4242a67db7c
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-11-20 15:41:37 +0700

    Enhance login page layout with background image and responsive design

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,20 @@
 import { LoginForm } from "@/components/Login-form";
-import useAuthRedirect from '@/hooks/useAuthRedirect';
+import useAuthRedirect from "@/hooks/useAuthRedirect";
 
 export function Page() {
   useAuthRedirect();
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
-      <LoginForm />
+    <div className="flex min-h-screen w-full">
+      {/* Gambar di sebelah kiri */}
+      <div className="hidden md:flex w-1/2 bg-cover bg-center bg-[url(login.jpg)]">
+        {/* Gambar sebagai background */}
+      </div>
+
+      {/* Login form di sebelah kanan */}
+      <div className="flex w-full md:w-1/2 items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
   );
 }
